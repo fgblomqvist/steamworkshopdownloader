1856866b07173013a219b32d8b3bd8ef40da8cab
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2014-08-08 13:46:18 +0200

    Replaced ngRoute with UI Router

--- steamworkshopdownloader/static/js/app.js
+++ steamworkshopdownloader/static/js/app.js
@@ -1,24 +1,25 @@
 'use strict';
 
-angular.module('steamWorkshopDownloaderApp', ['ngRoute'])
-	.config(['$routeProvider', '$locationProvider', '$interpolateProvider',
-		function($routeProvider, $locationProvider, $interpolateProvider) {
+angular.module('steamWorkshopDownloaderApp', ['ui.router'])
+	.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$interpolateProvider',
+		function($urlRouterProvider, $stateProvider, $locationProvider, $interpolateProvider) {
 
         // change the tags so they don't conflict with Jinja2 template tags
         $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 
-		$routeProvider
-		.when('/', {
+        $urlRouterProvider.otherwise('/');
+
+		$stateProvider
+		.state('home', {
+            url: '/',
 			templateUrl: 'static/partials/home.html',
 			controller: IndexController
 		})
-		.when('/about', {
+		.state('about', {
+            url: '/about',
 			templateUrl: 'static/partials/about.html',
 			controller: AboutController
-		})
-		.otherwise({
-			redirectTo: '/'
 		});
 
-		$locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 	}]);
