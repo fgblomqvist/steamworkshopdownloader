383486f49b8305934d75875a016bffdba8ded31c
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2014-08-18 14:33:03 +0200

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+ 
+  grunt.registerTask('watch', [ 'watch' ]);
+  grunt.registerTask('production', ['less:production']);
+  grunt.registerTask('default', ['less:dev']);
+
+  grunt.initConfig({
+    less: {
+      production: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          "steamworkshopdownloader/static/css/style.min.css": "less/style.less"
+        }
+      },
+      dev: {
+        options: {
+        },
+        files: {
+          "steamworkshopdownloader/static/css/style.min.css": "less/style.less"
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['less/**/*.less'],
+        tasks: ['less:dev'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+ 
+};
