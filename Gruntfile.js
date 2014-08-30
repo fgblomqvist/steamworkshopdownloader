e057f6e5016a7c595a8a867170229960059f9564
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2014-08-31 00:39:34 +0200

    Added Flask-versioned with hash based cache busting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,60 @@
-module.exports = function(grunt) {
- 
-  grunt.registerTask('watch', [ 'watch' ]);
-  grunt.registerTask('production', ['less:production']);
-  grunt.registerTask('default', ['less:dev']);
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    less: {
-      production: {
-        options: {
-          cleancss: true
-        },
-        files: {
-          "steamworkshopdownloader/static/css/style.min.css": "less/style.less"
-        }
-      },
-      dev: {
-        options: {
-        },
-        files: {
-          "steamworkshopdownloader/static/css/style.min.css": "less/style.less"
-        }
-      }
-    },
-    watch: {
-      css: {
-        files: ['less/**/*.less'],
-        tasks: ['less:dev'],
-        options: {
-          livereload: true
-        }
-      },
-      html: {
-        files: ['steamworkshopdownloader/static/partials/**/*.html',
-                'steamworkshopdownloader/templates/**/*.html'],
-        options: {
-          livereload: true
-        }
-      },
-      js: {
-        files: ['steamworkshopdownloader/static/js/**/*.js'],
-        options: {
-          livereload: true
-        }
-      }
-    }
-  });
+    grunt.registerTask('watch', [ 'watch' ]);
+    grunt.registerTask('production', ['less:production', 'cachebuster']);
+    grunt.registerTask('default', ['less:dev', 'cachebuster']);
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
- 
+    grunt.initConfig({
+        cachebuster: {
+            options: {
+                format: 'json',
+                basedir: 'steamworkshopdownloader'
+            },
+            'steamworkshopdownloader/static/cachebusters.json': ['steamworkshopdownloader/static/css/style.min.css']
+        },
+        less: {
+            production: {
+                options: {
+                    cleancss: true
+                },
+                files: {
+                    "steamworkshopdownloader/static/css/style.min.css": "less/style.less"
+                }
+            },
+            dev: {
+                options: {
+                },
+                files: {
+                    "steamworkshopdownloader/static/css/style.min.css": "less/style.less"
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['less/**/*.less'],
+                tasks: ['less:dev', 'cachebuster'],
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: ['steamworkshopdownloader/static/partials/**/*.html',
+                    'steamworkshopdownloader/templates/**/*.html'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['steamworkshopdownloader/static/js/**/*.js'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-cachebuster');
+
 };
